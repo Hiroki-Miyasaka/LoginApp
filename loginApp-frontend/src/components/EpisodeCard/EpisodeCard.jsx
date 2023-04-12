7a71e0c5ea9e3f231ede6d6221f52036daf204a7
Hiroki <h.m.jojo.8258@gmail.com>
2023-04-11 17:11:07 -0700

    made location and episode card

--- loginApp-frontend/src/components/EpisodeCard/EpisodeCard.jsx
+++ loginApp-frontend/src/components/EpisodeCard/EpisodeCard.jsx
@@ -4,7 +4,22 @@ import { useNavigate } from 'react-router-dom';
 
 
 const EpisodeCardContainer = styled.div`
-
+    border: solid #ffffff 1px;
+    border-radius: 5px;
+    h3 {
+        margin: 1rem 0;
+    }
+    p {
+        margin: 0.2rem 0;
+    }
+    button {
+        border:none;
+        padding: 10px 12px;
+        border-radius: 8px;
+        background: #ffffff;
+        cursor: pointer;
+        margin: 1rem 0;
+    }
 `;
 
 const EpisodeCard = ({ air_date, characters, created, name}) => {
@@ -15,6 +30,7 @@ const EpisodeCard = ({ air_date, characters, created, name}) => {
             <h3>Episode: {name}</h3>
             <p>Air Date: {air_date}</p>
             <p>Created: {created}</p>
+            <button>Appeared Characters</button>
         </EpisodeCardContainer>
     )
 }
--- loginApp-frontend/src/components/LocationCard/LocationCard.jsx
+++ loginApp-frontend/src/components/LocationCard/LocationCard.jsx
@@ -4,10 +4,27 @@ import { useNavigate } from 'react-router-dom';
 
 
 const LocationCardContainer = styled.div`
-
+    border: solid #ffffff 1px;
+    border-radius: 5px;
+    h3 {
+        margin: 1rem 0;
+    }
+    p {
+        margin: 0.2rem 0;
+    }
+    button {
+        border:none;
+        padding: 10px 12px;
+        border-radius: 8px;
+        background: #ffffff;
+        cursor: pointer;
+        margin: 1rem 0;
+    }
 `;
 
 const LocationCard = ({ created, dimension, name, residents, type }) => {
+
+    // const navigate = useNavigate();
 
     return(
         <LocationCardContainer>
@@ -15,6 +32,7 @@ const LocationCard = ({ created, dimension, name, residents, type }) => {
             <p>Type: {type}</p>
             <p>Created: {created}</p>
             <p>Dimension: {dimension}</p>
+            <button>Residents</button>
         </LocationCardContainer>
     )
 }
